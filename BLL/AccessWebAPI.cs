5df0d20129dcf358e41d46e1d723d9433cb2d965
Flavien Bonvin <bonvin.flavien@gmail.com>
2017-06-06 10:39:04 +0200

    First commit

---
+++ BLL/AccessWebAPI.cs
@@ -0,0 +1,69 @@
+﻿using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BLL
+{
+    public class AccessWebAPI
+    {
+        private string baseUri = "http://localhost:2998/api/ProductsEF/";
+
+        public List<Client> getClients()
+        {
+            //want to access this link
+            string uri = baseUri;
+
+            HttpClient clientHTTP = new HttpClient();
+            //async is for access the link and do something else
+            //new thread
+            Task<string> response = clientHTTP.GetStringAsync(uri);
+            clientHTTP.Dispose();
+            return JsonConvert.DeserializeObject<List<Client>>(response.Result);
+
+        }
+
+        public bool PostClient(Client client)
+        {
+            string uri = baseUri;
+            using (HttpClient clientHTTP = new HttpClient())
+            {
+                string pro = JsonConvert.SerializeObject(client);
+                StringContent frame = new StringContent(pro, Encoding.UTF8, "Application/json");
+                Task<HttpResponseMessage> reponse = clientHTTP.PostAsync(uri, frame);
+                return reponse.Result.IsSuccessStatusCode;
+            }
+
+        }
+
+        /*
+        public bool PutProducts(Product p)
+        {
+            string uri = baseUri;
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string pro = JsonConvert.SerializeObject(p);
+                StringContent frame = new StringContent(pro, Encoding.UTF8, "Application/json");
+                Task<HttpResponseMessage> reponse = httpClient.PutAsync(uri, frame);
+                return reponse.Result.IsSuccessStatusCode;
+            }
+
+        }
+        
+
+        public bool DeleteProducts(int i)
+        {
+            string uri = baseUri;
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<HttpResponseMessage> reponse = httpClient.DeleteAsync(uri);
+                return reponse.Result.IsSuccessStatusCode;
+            }
+        }
+        */
+    }
+}
---
+++ DTO/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public String Lastname { get; set; }
+        public String Fistname { get; set; }
+        public String Email { get; set; }
+    }
+}
